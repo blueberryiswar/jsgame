db0cc244a8dd8be3085062aea3287d03ed08381b
blueberryiswar <cudgeon@gmail.com>
2016-10-23 10:36:06 +0200

    resize canvas, added aliases

--- js/main.js
+++ js/main.js
@@ -1,14 +1,36 @@
 //Test that Pixi is working
-console.log(PIXI);
+console.log("Welcome to Cheeky Demons.");
+
+
+
+//Aliases
+var Container = PIXI.Container,
+    autoDetectRenderer = PIXI.autoDetectRenderer,
+    loader = PIXI.loader,
+    resources = PIXI.loader.resources,
+    Sprite = PIXI.Sprite;
+
+
+//Variables
+var windowWidth,
+    windowHeight;
 
 //Create the renderer
-var renderer = PIXI.autoDetectRenderer(256, 256);
+var renderer = autoDetectRenderer(256, 256);
 
 //Add the canvas to the HTML document
 document.body.appendChild(renderer.view);
 
+renderer.view.style.position = "absolute";
+renderer.view.style.display = "block";
+renderer.autoResize = true;
+windowWidth = window.innerWidth;
+windowHeight = window.innerHeight;
+renderer.resize(window.innerWidth, window.innerHeight);
+
 //Create a container object called the `stage`
-var stage = new PIXI.Container();
+var stage = new Container();
 
 //Tell the `renderer` to `render` the `stage`
 renderer.render(stage);
+
